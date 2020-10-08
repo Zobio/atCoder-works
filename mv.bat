df45b6f2b783be4a23c3af8c29d7bac66820607d
Zobio <soshinito514@gmail.com>
2020-10-08 17:03:16 +0900

    added bat file for C# and abc153a

---
+++ mv.bat
@@ -0,0 +1,5 @@
+set /P name="Press the name  "
+echo;
+copy C:\Users\soshi\source\repos\ConsoleApp1\ConsoleApp1\Program.cs C:\Users\soshi\Desktop\atCoder-works\work
+ren C:\Users\soshi\Desktop\atCoder-works\work\Program.cs %name%.cs
+pause
---
+++ work/abc153a.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = (Console.ReadLine().Split(' '));
+            int[] h = new int[2];
+            h[0] = int.Parse(a[0]);
+            h[1] = int.Parse(a[1]);
+            Console.WriteLine(h[0] % h[1] == 0 ? h[0] / h[1] : h[0] / h[1] + 1);
+        }
+    }
+}
