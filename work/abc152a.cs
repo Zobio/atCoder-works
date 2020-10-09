242a8bbd872e4e439c0eba53db2f32f3534100fa
Zobio <soshinito514@gmail.com>
2020-10-09 17:45:33 +0900

    abc152ab

---
+++ work/abc152a.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] a = (Console.ReadLine().Split(' '));
+            Console.WriteLine(int.Parse(a[0]) == int.Parse(a[1]) ? "Yes" : "No");
+        }
+    }
+}
---
+++ work/abc152b.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] str = (Console.ReadLine().Split(' '));
+            int a = int.Parse(str[0]);
+            int b = int.Parse(str[1]);
+            for(int i = 0; i < Math.Max(a, b); i++)
+            {
+                Console.Write(Math.Min(a, b));
+            }
+        }
+    }
+}
