0b2dae8a03fcf5ba9852273ecc4d5f1a1e48fe07
Zobio <soshinito514@gmail.com>
2020-11-19 23:58:48 +0900

    first cpp

---
+++ work/abc151a.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+	char a = getchar();
+	putchar(a + 1);
+}
---
+++ work/abc151a.cpp
@@ -0,0 +1,6 @@
+using namespace cstdio
+
+int main(void) {
+	char a = getchar();
+	putchar(a + 1);
+}
