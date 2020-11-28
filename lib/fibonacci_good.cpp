7f5f056fea2df56d25d5a09113da1193dc5b773f
Zobio <soshinito514@gmail.com>
2020-11-29 02:14:03 +0900

    fibonacciライブラリ abs184d

---
+++ lib/fibonacci_good.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<long long> memo(50, -1);
+
+int cnt = 0;
+long long fibonacci(int n) {
+  cnt++;
+  if (memo[n] != -1) return memo[n];
+  return memo[n] = fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+int main() {
+  int n; cin >> n;
+  memo[0] = 0;
+  memo[1] = 1;
+  cout << fibonacci(n);
+  return 0;
+}
--- work/ans184d2.cpp
+++ work/ans184d2.cpp
@@ -6,10 +6,14 @@ int a,b,c,cnt;
 signed main()
 {
 	scanf("%lld%lld%lld",&a,&b,&c);
-	for(int i=99;i>=a;i--)
-	for(int j=99;j>=b;j--)
-	for(int k=99;k>=c;k--)
-	f[i][j][k]=1 + (f[i][j][k+1] * k + f[i][j+1][k] * j + f[i+1][j][k] * i)/(i + j + k);
+	for(int i=99;i>=a;i--) {
+		for(int j=99;j>=b;j--) {
+			for(int k=99;k>=c;k--) {
+				f[i][j][k]=1 + (f[i][j][k+1] * k + f[i][j+1][k] * j + f[i+1][j][k] * i)/(i + j + k);
+				printf("f[%lld][%lld][%lld]: = %f\n", i, j, k, f[i][j][k]);
+			}
+		}
+	}
 	printf("%.9lf",f[a][b][c]);
     return 0;
 }
