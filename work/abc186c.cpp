64542797805985a562184581e61b0a8796281c95
Zobio <soshinito514@gmail.com>
2020-12-20 00:42:34 +0900

    abc186c

--- work/abc186c.cpp
+++ work/abc186c.cpp
@@ -1,19 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string to_oct(int n) {
+	string s;
+	while (n) {
+		s = to_string(n % 8) + s;
+		n /= 8;
+	}
+	return s;
+}
+
 int main() {
 	int n;
 	cin >> n;
-	int nCopy = n;
-	int k8 = 0, k10 = 0;
-	while(nCopy >= 10) {
-		nCopy /= 10;
-		k10++;
+	int cnt = 0;
+	for(int i = 1; i <= n; i++) {
+		if (to_string(i).find("7") == -1 && to_oct(i).find("7") == -1) {
+			cnt++;
+		}
 	}
-	nCopy = n;
-	while(nCopy >= 8) {
-		nCopy /= 8;
-		k8++;
-	}
-	
+	cout << cnt << endl;
 }
--- work/test.cpp
+++ work/test.cpp
@@ -4,9 +4,6 @@ using namespace std;
 
 int main()
 {
-	string s1 = "testtest";
-	string s2 = "";
-	string s3 = "TEST";
-
-	cout << "s1=" << s1 << ", s2=" << s2 << ", s3=" << s3 << endl;
+	string a = "Hello, world!";
+	printf("%s", a);
 }
