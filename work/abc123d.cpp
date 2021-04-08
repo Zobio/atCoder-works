4fd4f99eeaaa5950fc2722c8e1905eb8b7994eff
Zobio <soshinito514@gmail.com>
2021-04-08 20:33:27 +0900

    abc123d

--- work/abc123d.cpp
+++ work/abc123d.cpp
@@ -24,4 +24,17 @@ int main() {
 	sort(a.rbegin(), a.rend());
 	sort(b.rbegin(), b.rend());
 	sort(c.rbegin(), c.rend());
+	vector<ll> lst;
+	for(int i = 0; i < x; i++) {
+		for(int j = 0; j < y; j++) {
+			for(int l = 0; l < z; l++) {
+				if((i + 1) * (j + 1) * (l + 1) > k) break;
+				lst.push_back(a[i] + b[j] + c[l]);
+			}
+		}
+	}
+	sort(lst.rbegin(), lst.rend());
+	for(int i = 0; i < min(k, (int)lst.size()); i++) {
+		cout << lst[i] << endl;
+	}
 }
