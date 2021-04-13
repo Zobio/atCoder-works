f970083c957b562722d4a4d7c7cccccc5ff41a04
Zobio <soshinito514@gmail.com>
2021-04-13 16:42:38 +0900

    abc198d

--- work/abc121d.cpp
+++ work/abc121d.cpp
@@ -4,19 +4,39 @@ using namespace std;
 #define ull unsigned long long
 #define MOD 1000000007
 
+
 int main() {
 	ll a, b;
 	cin >> a >> b;
-	ll xorA_1 = 0;
-	if(a & 1) {
-		if((a / 2) & 1) {
-			xorA_1 = 1;
+	if(b - a == 1) {
+		cout << (a ^ b) << endl;
+		return 0;
+	}
+	else if(a == b) {
+		cout << a << endl;
+		return 0;
+	}
+	ll start, end;
+	vector<ll> lst;
+	for(ll i = a;; i++) {
+		if(i % 2 == 0) {
+			start = i;
+			break;
 		}else{
-			xorA_1 = 0;
-		}
-	}else{
-		if((a / 2) & 1) {
-			xorA_1 = 1 ^ 
+			lst.push_back(i);
 		}
 	}
+	for(ll i = b;; i--) {
+		if(i % 2) {
+			end = i;
+			break;
+		}else{
+			lst.push_back(i);
+		}
+	}
+	ll ans = ((end - start + 1) / 2) % 2 ? 1 : 0;
+	for(ll i = 0; i < lst.size(); i++) {
+		ans ^= lst[i];
+	}
+	cout << ans << endl;
 }
--- work/abc198d2.cpp
+++ work/abc198d2.cpp
@@ -4,8 +4,48 @@ using namespace std;
 #define ull unsigned long long
 #define MOD 1000000007
 
+vector<string> s(3), ss(3);
+vector<bool> word(26, false), yet(10, false);
+vector<int> n(26);
+
+void dfs(int x) {
+	if(x == 26) {
+		ss[0] = ss[1] = ss[2] = "";
+		for(int i = 0; i < 3; i++) {
+			for(int j = 0; j < s[i].size(); j++) {
+				ss[i] += n[s[i][j] - 'a'] + '0';
+			}
+		}
+		if(ss[0][0] == '0' || ss[1][0] == '0' || ss[2][0] == '0') return;
+		if(stoll(ss[0]) + stoll(ss[1]) == stoll(ss[2])) {
+			cout << ss[0] << endl << ss[1] << endl << ss[2] << endl;
+			exit(0);
+		}
+		return;
+	}
+	if(!word[x]){dfs(x+1);return;}
+	for(int i = 0; i < 10; i++) {
+		if(yet[i]) continue;
+		n[x] = i;
+		yet[i] = true;
+		dfs(x + 1);
+		yet[i] = false;
+	}
+}
+
 int main() {
-	vector<string> s(3);
 	cin >> s[0] >> s[1] >> s[2];
-	
+	for(int i = 0; i < 3; i++) {
+		for(int j = 0; j < s[i].size(); j++) {
+			word[s[i][j] - 'a'] = true;
+		}
+	}
+	for(int i = 0; i < 26; i++) {
+		if(word[i]) {
+			dfs(i);
+			break;
+		}
+	}
+	puts("UNSOLVABLE");
+	return 0;
 }
