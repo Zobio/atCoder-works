88fd29d07227aef77694ea947af925775b4d5b08
Zobio <soshinito514@gmail.com>
2021-05-31 22:37:55 +0900

    dpF(途中)

--- work/dpF.cpp
+++ work/dpF.cpp
@@ -4,20 +4,40 @@ using namespace std;
 int main() {
 	string s, t;
 	cin >> s >> t;
-	int slen = s.length(), tlen = t.length();
-	vector<bool> yetS(slen, true);
-	vector<bool> yetT(tlen, true);
-	string ans = "";
-	for(int i = 0; i < slen; i++) {
-		for(int j = 0; j < tlen; j++) {
-			if(!yetS[i] || !yetT[j]) continue;
+	vector<vector<int>> dp(s.length() + 1, vector<int>(t.length() + 1));
+	for(int i = 0; i < s.length(); i++) {
+		for(int j = 0; j < t.length(); j++) {
 			if(s[i] == t[j]) {
-				cout << i << " " << j << " " << s[i] << endl;
-				ans += s[i];
-				yetS[i] = false;
-				yetT[j] = false;
+				dp[i + 1][j + 1] = dp[i][j] + 1;
+			}else{
+				dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
 			}
 		}
 	}
+	for(int i = 0; i <= s.length(); i++) {
+		for(int j = 0; j <= t.length(); j++) {
+			cout << dp[i][j];
+		}
+		cout << endl;
+	}
+	string ans = "";
+	int len = dp[s.length()][t.length()];
+	int slen = s.length(), tlen = t.length();
+	while(len + 1) {
+		cout << slen << " " << s[slen] << " " << tlen << " " << t[tlen] << endl;
+		if(s[slen] == t[tlen]) {
+			ans += s[slen];
+			slen--;
+			tlen--;
+			len--;
+			cout << "len: " << len << endl;
+		}
+		else if(dp[slen][tlen] == dp[slen - 1][tlen] && slen - 1 >= 0) {
+			slen--;
+		}else{
+			tlen--;
+		}
+	}
+	reverse(ans.begin(), ans.end());
 	cout << ans << endl;
 }
