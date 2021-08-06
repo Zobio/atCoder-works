20901d2fb526975b043d8c0aa619012f40a1511c
Zobio <soshinito514@gmail.com>
2021-08-06 15:35:27 +0900

    abc171c(途中)

--- work/abc171c.cpp
+++ work/abc171c.cpp
@@ -6,22 +6,28 @@ using namespace std;
 #define MOD 1000000007
 #define ce(i) cout << i << endl
 
-int main() {
+int main()
+{
 	ll n;
 	cin >> n;
 	ll cnt = 1;
 	ll cp = n;
-	while(cp > (ll)(pow(26, cnt))){
-		cp -= (ll)(pow(26, cnt)), cnt++;
+	vector<ll> rui; rui.push_back(1);
+	ll now = 0;
+	while((ll)(pow(26, now)) <= (ll)1000000000000001) {
+		rui.push_back(rui[now] + (ll)(pow(26, now + 1)));
+		now++;
 	}
+	while (cp > (ll)(pow(26, cnt)))
+		cp -= (ll)(pow(26, cnt)), cnt++;
 	string ans = "";
 	char ch = 'a' - 1;
-	ll fix = 0;
-	for(ll i = cnt; i >= 1; i--) {
-		if(n - (ll)(pow(26, i - 1)) * (n / (ll)(pow(26, i - 1)) < (ll)(pow(26, i - 2) - 1))) fix = 1;
-		ans += (char)(ch + n / (ll)(pow(26, i - 1)) - fix);
-		n -= (ll)(pow(26, i - 1)) * (n / (ll)(pow(26, i - 1)) - fix);
-		fix = 0;
+	for (ll i = cnt; i >= 1; i--) {
+		ll fix = 0;
+		if (i != 1)if((n - rui[i - 2]) % (ll)(pow(26, i - 1)) * (n / (ll)(pow(26, i - 1))) < rui[i - 2]) fix = 1;
+		cout << (n - rui[i - 2]) / (ll)(pow(26, i - 1)) - fix << " " << (n - rui[i - 2]) % (ll)(pow(26, i - 1)) * ((n - rui[i - 2]) / (ll)(pow(26, i - 1))) << " " << rui[i - 2] << endl;
+		ans += (char)(ch + (n - rui[i - 2]) / (ll)(pow(26, i - 1)) - fix);
+		n -= (ll)(pow(26, i - 1)) * ((n - rui[i - 2]) / (ll)(pow(26, i - 1)) - fix);
 	}
 	cout << ans << endl;
 }
