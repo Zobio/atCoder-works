a469f95a5106f52a85d3326e5380a97cad2dd4f9
Zobio <soshinito514@gmail.com>
2021-08-08 13:23:11 +0900

    二分探索ライブラリ

--- lib/binary_search.cpp
+++ lib/binary_search.cpp
@@ -1,5 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define ll long long
+#define ull unsigned long long
+#define MOD 1000000007
+#define co(i) cout << i << endl
+
 int binary_search(int arr[], int arr_len, int x) {
     int l = -1;
     int r = arr_len - 1;
@@ -24,4 +30,24 @@ int main() {
     cout << binary_search(arr, arr_len, 4) << endl; // 出力は1
     cout << binary_search(arr, arr_len, 1000) << endl; // 出力は-1
     return 0;
+}
+
+int main2() {
+    //左側が条件を満たす
+    ll left = 0, right = 1e10;
+	while(right - left > 1) {
+		ll mid = (right + left) / 2;
+		if(ok(mid)) left = mid;
+		else right = mid;
+	}
+	cout << left << endl;
+
+    //右側が条件を満たす
+    ll left = 0, right = 1e10;
+	while(right - left > 1) {
+		ll mid = (left + right) / 2;
+		if(ok(mid)) right = mid;
+		else left = mid;
+	}
+	cout << right << endl;
 }
