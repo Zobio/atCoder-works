4e4e648524c8b5c79e8df928c19d40c747d7651b
Zobio <soshinito514@gmail.com>
2021-10-10 14:34:15 +0900

    hack_to_the_future 途中まで

--- work/test.cpp
+++ work/test.cpp
@@ -17,45 +17,43 @@ using namespace std;
 template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
-struct Edge {
-    long long to;
-    long long cost;
-};
-using Graph = vector<vector<Edge>>;
-using P = pair<long, int>;
-const long long INF = 1LL << 60;
-/* dijkstra(G,s,dis)
-    入力：グラフ G, 開始点 s, 距離を格納する dis
-    計算量：O(|E|log|V|)
-    副作用：dis が書き換えられる
-*/
-void dijkstra(const Graph &G, int s, vector<long long> &dis) {
-    int N = G.size();
-    dis.resize(N, INF);
-    priority_queue<P, vector<P>, greater<P>> pq;  // 「仮の最短距離, 頂点」が小さい順に並ぶ
-    dis[s] = 0;
-    pq.emplace(dis[s], s);
-    while (!pq.empty()) {
-        P p = pq.top();
-        pq.pop();
-        int v = p.second;
-        if (dis[v] < p.first) {  // 最短距離で無ければ無視
-            continue;
-        }
-        for (auto &e : G[v]) {
-            if (dis[e.to] > dis[v] + e.cost) {  // 最短距離候補なら priority_queue に追加
-                dis[e.to] = dis[v] + e.cost;
-                pq.emplace(dis[e.to], e.to);
-            }
-        }
+template<typename V>
+void shrink_coordinates(V& a) {
+    /*
+    座標圧縮を行う関数
+    (例)
+    {6, 9, 9, 2, 100} -->  {1, 2, 2, 0, 3}
+    */
+    V b = a;
+    sort(b.begin(), b.end());
+    b.erase(unique(b.begin(), b.end()), b.end());
+    V res(a.size());
+    for(ll i = 0; i < a.size(); i++) {
+        res.at(i) = lower_bound(b.begin(), b.end(), a.at(i)) - b.begin();
     }
+    a = res;
+}
+
+template<typename VV>
+void rotate_90(VV& a) {
+    /*2次元グリッドを反時計回りに90°回転させる関数 (2回回せば逆さまになる)
+    (例)
+    000     011
+    001 --> 000
+    101     001
+    注意 : 縦と横のサイズが同じでないとバグる
+    */
+    size_t siz = a.size();
+    VV b(siz);
+    for(size_t i = 0; i < siz; i++) for(size_t j = 0; j < siz; j++) b[i].push_back(a[j][siz - i - 1]);
+    a = b;
 }
 
 int main() {
-  ll n, m; cin >> n >> m;
-  Graph g(n);
-  rep(i, m) {
-    ll a, b, c; cin >> a >> b >> c; a--; b--;
-    g.push_back(make_pair)
-  }
+    ll n; cin >> n;
+    vector<ll> a(n);
+    rep(i, n) cin >> a[i];
+    rep(i, n) cout << a[i] << " "; cout << endl;
+    shrink_coordinates(a);
+    rep(i, n) cout << a[i] << " "; cout << endl;
 }
