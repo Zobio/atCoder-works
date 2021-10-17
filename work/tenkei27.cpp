89dc4459903fc539461024f4b4ce63881899a395
Zobio <soshinito514@gmail.com>
2021-10-17 23:28:39 +0900

    典型90 27

---
+++ work/tenkei27.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define all(a) (a).begin(), (a).end()
+#define MOD 998244353LL
+#define INF 1LL << 60
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	ll n; cin >> n;
+	map<string, bool> mp;
+	rep(i, n) {
+		string s; cin >> s;
+		if(!mp[s]) cout << i + 1 << endl, mp[s] = true;
+	}
+}
