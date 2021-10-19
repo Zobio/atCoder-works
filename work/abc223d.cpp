b185fe9188319df0dce514dd53f7d1da366938e5
Zobio <soshinito514@gmail.com>
2021-10-19 15:39:23 +0900

    abc223d

--- work/abc223d.cpp
+++ work/abc223d.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define ll long long
 #define ull unsigned long long
+#define ld long double
 #define rep(i, n) for (long long i = 0; i < n; i++)
 #define reps(i, n) for (long long i = 1; i <= n; i++)
 #define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
@@ -17,75 +18,28 @@ using namespace std;
 template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
-struct UnionFind {
-	ll groups;
-	vector<ll> parents;
-
-	UnionFind(ll n) {
-		groups = n;
-		parents = vector<ll>(n, -1);
-	}
-
-	ll find(ll x) {
-		if (parents.at(x) < 0) {
-			return x;
-		}else{
-			parents[x] = find(parents[x]);
-			return parents[x];
-		}
-	}
-
-	void unite(ll x, ll y) {
-		x = find(x);
-		y = find(y);
-
-		// already united
-		if (x == y)
-			return;
-
-		groups--;
-
-		if (parents[x] > parents[y])
-			swap(x, y);
-
-		parents[x] += parents[y];
-		parents[y] = x;
-	}
-
-	ll size(ll x) {
-		return -parents[find(x)];
-	}
-
-	bool issame(ll x, ll y) {
-		return find(x) == find(y);
-	}
-
-	vector<ll> roots() {
-		vector<ll> ret;
-		for (ll i = 0; i < parents.size(); i++)
-			if (parents[i] < 0)
-				ret.push_back(i);
-		return ret;
-	}
-
-	ll group_count() {
-		return groups;
-	}
-};
-
 int main() {
 	ll n, m; cin >> n >> m;
-	deque<ll> d; rep(i, n) d.push_back(i + 1);
-	vector<pair<ll, ll>> ab(m);
-	UnionFind uf(n);
+	vvll g(n);
 	rep(i, m) {
-		ll a, b; cin >> a >> b;
-		ab[i] = {a, b};
-		uf.unite(a, b);
+		ll a, b; cin >> a >> b; a--; b--;
+		g[a].push_back(b);
 	}
-	rep(i, m) {
-		ll a = ab[i].first, b = ab[i].second;
-		if(a < b && mp[{b, a}] == false) continue;
-		else if(a < b && mp[{b, a}] == true) {cout << -1 << endl; return 0;}
+	priority_queue<ll, vector<ll>, greater<ll>> pq;
+	vll cnt(n);
+	rep(i, n) rep(j, g[i].size()) cnt[g[i][j]]++;
+	rep(i, n) {
+		if(cnt[i] == 0) pq.push(i);
 	}
+	vll ans;
+	while(!pq.empty()) {
+		ll now = pq.top(); pq.pop();
+		ans.push_back(now + 1);
+		for(auto a : g[now]) {
+			cnt[a]--;
+			if(cnt[a] == 0)pq.push(a);
+		}
+	}
+	if(ans.size() < n) cout << -1 << endl;
+	else arrcout(ans);
 }
