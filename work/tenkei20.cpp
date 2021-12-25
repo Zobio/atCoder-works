8785df11278fd0970f3b12833a6cc3f7291d0aba
Zobio <soshinito514@gmail.com>
2021-12-25 20:28:35 +0900

    tenkei20

---
+++ work/tenkei20.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF 1LL << 60
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+template<typename T>
+T mpow(T a, T n, T m) {
+	/*a^n % mを返す
+	(例)
+	pow(2, 10, 1000) --> 24
+	計算量はlog(n)
+	*/
+	T ret = 1;
+	while(n > 0) {
+		if (n & 1) ret = ret * a % m;
+		a = a * a % m;
+		n >>= 1;
+	}
+	return ret;
+}
+
+int main() {
+	ll a, b, c; cin >> a >> b >> c;
+	ll cmp = mpow(c, b, 9000000000000000010ll);
+	cout << (log2(a) < log2(cmp) ? "Yes" : "No") << endl;
+}
