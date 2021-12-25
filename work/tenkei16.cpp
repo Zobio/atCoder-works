7d4dfd14dd6748cf1d782b7f591e0aedd4621b0b
Zobio <soshinito514@gmail.com>
2021-12-25 11:18:43 +0900

    tenkei16(TLE)

---
+++ work/tenkei16.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF 1LL << 60
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	ll n, a, b, c; cin >> n >> a >> b >> c;
+	ll lim = 9999, ans = INF;
+	for(ll x = 0; x <= lim; x++) {
+		for(ll y = 0; x + y <= lim; y++) {
+			for(ll z = 0; x + y + z <= lim; z++) {
+				if(a * x + b * y + c * z == n) chmin(ans, x + y + z);
+			}
+		}
+	}
+	cout << ans << endl;
+}
