9dbca9e9b024c68dbeba47af99350a0b3eb8651d
Zobio <soshinito514@gmail.com>
2021-12-29 18:30:56 +0900

    tenkei64

--- work/tenkei64.cpp
+++ work/tenkei64.cpp
@@ -25,9 +25,27 @@ template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } 
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
 int main() {
-	ll n; cin >> n;
+	ll n, q; cin >> n >> q;
 	vll a(n); rep(i, n) cin >> a[i];
-	ll res = 0;
-	rep(i, n) res += abs(a[i]);
-	cout << res << endl;
+	vector<pair<pair<ll,ll>, ll>> change(q);
+	rep(i, q) {
+		ll l, r, v; cin >> l >> r >> v; l--; r--;
+		change[i] = {{l, r}, v};
+	}
+	ll ans = 0;
+	rep(i, n - 1) ans += abs(a[i] - a[i + 1]);
+	vll res;
+	for(auto ch : change) { //左右の差分を計算していってansを変えていく
+		ll l = ch.first.first, r = ch.first.second, v = ch.second;
+		if(l > 0) { //l == 0だったら左側で不便さの変化は起こらない
+			if((a[l - 1] <= a[l]) == (v >= 0)) ans += abs(v); //差が広がる
+			else ans -= abs(a[l] - v); //差が縮まる
+		}
+		if(r < n - 1) { //r == n - 1だったら右側で不便さの変化は起こらない
+			if((a[r] >= a[r + 1]) == (v >= 0)) ans += abs(v);//差が広がる
+			else ans -= abs(a[l] - v);//差が縮まる
+		}
+		res.push_back(ans);
+	}
+	for(auto au : res) cout << au << endl;
 }
--- work/tenkei64_2.cpp
+++ work/tenkei64_2.cpp
@@ -25,9 +25,19 @@ template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } 
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
 int main() {
-	ll n; cin >> n;
+	ll n, q; cin >> n >> q;
 	vll a(n); rep(i, n) cin >> a[i];
-	ll res = 0;
-	rep(i, n) res += abs(a[i]);
-	cout << res << endl;
+	vll l(q), r(q), v(q);
+	rep(i, q) cin >> l[i] >> r[i] >> v[i], l[i]--, r[i]--;
+	vll b(n - 1); //aの階差数列
+	rep(i, n - 1) b[i] = a[i + 1] - a[i];
+	ll ans = 0; rep(i, n - 1) ans += abs(b[i]);
+	rep(i, q) {
+		ll before = (l[i] > 0 ? abs(b[l[i] - 1]) : 0) + (r[i] < n - 1 ? abs(b[r[i]]) : 0);
+		if(l[i] > 0) b[l[i] - 1] += v[i];
+		if(r[i] < n - 1) b[r[i]] -= v[i];
+		ll after = (l[i] > 0 ? abs(b[l[i] - 1]) : 0) + (r[i] < n - 1 ? abs(b[r[i]]) : 0);
+		ans += after - before;
+		cout << ans << endl;
+	} 
 }
