14c2e55132ae308a6027f378040d44941922db95
Zobio <soshinito514@gmail.com>
2021-12-30 09:14:11 +0900

    tenkei69

--- work/tenkei69.cpp
+++ work/tenkei69.cpp
@@ -19,15 +19,30 @@ using namespace std;
 #define setcout(n) cout << setprecision(n) << fixed
 #define all(a) (a).begin(), (a).end()
 #define rall(a) (a).rbegin(), (a).rend()
-#define MOD 998244353LL
+#define MOD 1000000007ll
 #define INF 1LL << 60
 template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
-vll dx = {1, 0, -1, 0};
-vll dy = {0, 1, 0, -1};
+template<typename T>
+T mpow(T a, T n, T m) {
+	/*a^n % mを返す
+	(例)
+	pow(2, 10, 1000) --> 24
+	計算量はlog(n)
+	*/
+	T ret = 1;
+	while(n > 0) {
+		if (n & 1) ret = ret * a % m;
+		a = a * a % m;
+		n >>= 1;
+	}
+	return ret;
+}
 
 int main() {
-	rep(i, 4) cout << dx[i] << " " << dy[i] << endl;
-	rep(i, 4) cout << dx[i] << " " << dx[i ^ 1] << endl;
+	ll n, k; cin >> n >> k;
+	if(n == 1) cout << k << endl;
+	else if(n == 2) cout << k * (k - 1) % MOD << endl;
+	else cout << k * (k - 1) % MOD * mpow((k - 2), n - 2, MOD) % MOD << endl;
 }
