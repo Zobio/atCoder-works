08d85d893bc1709e5c1dc90341dd1d0864916f38
Zobio <64645053+Zobio@users.noreply.github.com>
2022-01-18 16:10:36 +0900

    自作問題フォルダを追加 two_square

---
+++ self_made/two_square.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 1000000007LL
+#define INF (1LL << 60)
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+/*
+問題
+正整数Nが与えられます。
+Nが平方数の和である場合はYesを、そうでない場合はNoを出力してください
+
+制約
+[easy mode] 1 <= N <= 10^14 --> 10^7まで全探索
+[hard mode] 1 <= N <= 10^18
+*/
+
+int main() {
+    ll n; cin >> n;
+  	reps(nn, n) {
+      for(ll i = 1; i * i < nn; i++) {
+          ll sq = sqrt(nn - i * i);
+          if(sq * sq + i * i == nn && i <= sq) cout << nn << " " << i << " " << sq << endl;
+      }
+    }
+}
