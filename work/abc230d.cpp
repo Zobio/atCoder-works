d3d049026d2364e079dfaa944721bfd0564332a8
Zobio <soshinito514@gmail.com>
2022-03-27 17:57:07 +0900

    abc172d 230d

---
+++ work/abc230d.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;} cout << endl
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF (1LL << 60)
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+bool cmp(pll a, pll b) {
+	return a.second < b.second;
+}
+
+int main() {
+	ll n, d; cin >> n >> d;
+	vpll lr(n); rep(i, n) cin >> lr[i].first >> lr[i].second, lr[i].first--, lr[i].second--;
+	sort(all(lr), cmp);
+	ll cur = 0, pos = lr[0].second, ans = 0; //番地、座標、答え
+	while(cur < n) {
+		while(cur < n && lr[cur].first - pos + 1 <= d) cur++;
+		ans++; 
+		if(cur != n) pos = lr[cur].second;
+	}
+	cout << ans << endl;
+}
