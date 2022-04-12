cbc74fcaa65e5ac065fd3356eab8007f8f10d23f
Zobio <soshinito514@gmail.com>
2022-04-12 20:31:04 +0900

    abc247e(尺取り)

---
+++ work/abc247e.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF (1LL << 60)
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	ll n, x, y; cin >> n >> x >> y;
+	vll a(n); rep(i, n) cin >> a[i];
+	vvll b; b.push_back({});
+	rep(i, n) {
+		if(y <= a[i] && a[i] <= x) b.back().push_back(a[i]);
+		else b.push_back({});
+	}
+	ll ans = 0;
+	for(auto au : b) {
+		if(au.empty()) continue;
+		vll cnt(x + 1);
+		ll r = 0;
+		cnt[au.front()]++;
+		rep(l, au.size()) {
+			while(r < au.size() && (cnt[x] == 0 || cnt[y] == 0)) {
+				r++;
+				if(r < au.size()) cnt[au[r]]++;
+			}
+			ans += au.size() - r;
+			cnt[au[l]]--;
+		}
+	}
+	cout << ans << endl;
+}
