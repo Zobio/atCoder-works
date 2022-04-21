fb3d6e1193fd80d1cad1d9e3341716b6d72a9da9
Zobio <soshinito514@gmail.com>
2022-04-21 23:07:22 +0900

    abc178e

---
+++ work/abc178e.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF (1LL << 60)
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	ll n; cin >> n;
+	/*
+	z_i = x_i + y_i, w_j = x_i - y_i とすると、
+	ans = max(|z_i - z_j|, |w_i - w_j|)であり、
+	z_iとw_iが1番大きく、z_jとw_jが1番小さいときansは最大になる。	
+	*/
+	ll zmin = INF, zmax = -INF, wmin = INF, wmax = -INF;
+	rep(_, n) {
+		ll x, y; cin >> x >> y;
+		chmin(zmin, x + y);
+		chmax(zmax, x + y);
+		chmin(wmin, x - y);
+		chmax(wmax, x - y);
+	}
+	cout << max(zmax - zmin, wmax - wmin) << endl;
+}
