246e8b4f97b199b2d989c23d81f1b194a191357f
Zobio <soshinito514@gmail.com>
2022-04-28 10:25:28 +0900

    abc160e

---
+++ work/abc160e.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF (1LL << 60)
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	ll x, y, a, b, c; cin >> x >> y >> a >> b >> c;
+	vll p(a), q(b), r(c);
+	rep(i, a) cin >> p[i];
+	rep(i, b) cin >> q[i];
+	rep(i, c) cin >> r[i];
+	sort(rall(p)); sort(rall(q)); sort(rall(r));
+	priority_queue<ll, vll, greater<ll>> pq;
+	rep(i, x) pq.push(p[i]);
+	rep(i, y) pq.push(q[i]);
+	rep(i, c) {
+		if(pq.top() > r[i]) break;
+		pq.push(r[i]); pq.pop();
+	}
+	ll ans = 0;
+	while(!pq.empty()) ans += pq.top(), pq.pop();
+	cout << ans << endl;
+}
