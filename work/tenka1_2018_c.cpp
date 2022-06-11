7a05e496cdae445fdd8d12565ccfb87f03bfb40a
Zobio <soshinito514@gmail.com>
2022-06-11 20:48:53 +0900

    codefestival_2016_qualC_a(やる)

---
+++ work/tenka1_2018_c.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF (1LL << 60)
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	ll n; cin >> n;
+	vll a(n); rep(i, n) cin >> a[i];
+	sort(all(a));
+	deque<ll> d; rep(i, n) d.push_back(a[i]);
+	vll b;
+	bool fr = true;
+	while(!d.empty()) {
+		if(fr) b.push_back(d.front()), d.pop_front();
+		else b.push_back(d.back()), d.pop_back();
+		fr = !fr;
+	}
+	vll c;
+	rep(i, b.size() - 1) c.push_back(abs(b[i + 1] - b[i]));
+	arrcout(c);
+}
--- work/test.cpp
+++ work/test.cpp
@@ -1,41 +1,44 @@
-#include <bits/stdc++.h>
-#include <atcoder/all>
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+//#pragma GCC optimize ("-O3")
 using namespace std;
-using namespace atcoder;
-#define uint unsigned int
-#define ll long long
-#define ull unsigned long long
-#define ld long double
-#define rep(i, n) for (long long i = 0; i < n; i++)
-#define reps(i, n) for (long long i = 1; i <= n; i++)
-#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
-#define rreps(i, n) for (long long i = n; i >= 1; i--)
-#define fore(i, a) for (auto& i : a)
-#define vll vector<long long>
-#define vvll vector<vector<long long>>
-#define vvvll vector<vector<vector<long long>>>
-#define vvvvll vector<vector<vector<vector<long long>>>>
-#define pll pair<long long, long long>
-#define vpll vector<pair<long long, long long>>
-#define vvpll vector<vector<pair<long long, long long>>>
-#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
-#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
-#define setcout(n) cout << setprecision(n) << fixed
-#define all(a) (a).begin(), (a).end()
-#define rall(a) (a).rbegin(), (a).rend()
-#define MOD 998244353LL
-#define INF (1LL << 60)
-//#pragma GCC target("avx2")
-//#pragma GCC optimize("O3")
-//#pragma GCC optimize("unroll-loops")
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
 template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
-int main() {
-    set<ll> s;
-    s.insert(0); s.insert(10); s.insert(20);
-    set<ll>::iterator it = s.end(); 
-    it--;
-    *it--;
-    for(auto au : s) cout << au << " "; cout << endl;
+int N, A[101010], B[101010];
+//---------------------------------------------------------------------------------------------------
+ll get() {
+    ll sm = 0;
+    rep(i, 0, N - 1) sm += abs(B[i + 1] - B[i]);
+    return sm;
+}
+//---------------------------------------------------------------------------------------------------
+void _main() {
+    cin >> N;
+    rep(i, 0, N) cin >> A[i];
+    sort(A, A + N);
+ 
+    ll ans = -1;
+    if (N % 2 == 0) {
+        rep(i, 0, N / 2) B[i * 2 + 1] = A[i];
+        rep(i, 0, N / 2) B[i * 2] = A[i + N / 2];
+        ans = get();
+    } else {
+        rep(i, 0, N / 2) B[i * 2 + 1] = A[i];
+        B[N-1] = A[N/2];
+        rep(i, 0, N / 2) B[i * 2] = A[i + N / 2 + 1];
+        chmax(ans, get());
+ 
+        B[0] = A[N / 2];
+        rep(i, 0, N / 2) B[i * 2 + 2] = A[i];
+        rep(i, 0, N / 2) B[i * 2 + 1] = A[i + N / 2 + 1];
+        chmax(ans, get());
+    }
+    
+    cout << ans << endl;
 }
