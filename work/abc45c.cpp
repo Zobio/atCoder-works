5b4aa123f2b7aa1182ef21b8a87b750d7354b8b1
Zobio <soshinito514@gmail.com>
2022-06-11 13:04:25 +0900

    abc45c(全探索)

---
+++ work/abc45c.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF (1LL << 60)
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	string s; cin >> s;
+	ll n = s.size();
+	vll a;
+	rep(i, n) a.push_back(s[i] - '0');
+	ll ans = 0;
+	rep(bits, 1ll << (n - 1)) {
+		ll cur = a.front(), sum = 0;
+		rep(i, n - 1) {
+			if((bits >> i) & 1) sum += cur, cur = 0;
+			cur *= 10, cur += a[i + 1];
+		}
+		if(cur) sum += cur; //末端処理
+		ans += sum;
+	}
+	cout << ans << endl;
+}
