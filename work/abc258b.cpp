5e7102134158a34a4965c5ba8613e27ef20e5afa
Zobio <soshinito514@gmail.com>
2022-07-07 23:25:16 +0900

    abc257c 258abc

---
+++ work/abc258b.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF (1LL << 60)
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+vll dy = {-1, 0, 1};
+vll dx = {-1, 0, 1};
+
+int main() {
+	ll n; cin >> n;
+	vector<string> s(n); rep(i, n) cin >> s[i];
+	vvll a(n, vll(n)); rep(i, n) rep(j, n) a[i][j] = s[i][j] - '0';
+	ll ans = 0;
+	rep(i, n) rep(j, n) rep(p, 3) rep(q, 3) {
+		if(p == 1 && q == 1) continue;
+		ll cur = 0;
+		rep(l, n) {
+			ll ny = (i + dy[p] * l + n) % n;
+			ll nx = (j + dx[q] * l + n) % n;
+			cur *= 10; cur += a[ny][nx];
+		}
+		chmax(ans, cur);
+	}
+	cout << ans << endl;
+}
