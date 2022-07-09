1a120da375918efaee5635a3de8550fe9c659735
Zobio <soshinito514@gmail.com>
2022-07-09 20:46:15 +0900

    agc45b(Ad-Hoc)

--- work/test.cpp
+++ work/test.cpp
@@ -1,36 +1,37 @@
-#include <bits/stdc++.h>
-#include <atcoder/all>
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+//#pragma GCC optimize ("-O3")
 using namespace std;
-using namespace atcoder;
-#define uint unsigned int
-#define ll long long
-#define ull unsigned long long
-#define ld long double
-#define rep(i, n) for (long long i = 0; i < n; i++)
-#define reps(i, n) for (long long i = 1; i <= n; i++)
-#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
-#define rreps(i, n) for (long long i = n; i >= 1; i--)
-#define fore(i, a) for (auto& i : a)
-#define vll vector<long long>
-#define vvll vector<vector<long long>>
-#define vvvll vector<vector<vector<long long>>>
-#define vvvvll vector<vector<vector<vector<long long>>>>
-#define pll pair<long long, long long>
-#define vpll vector<pair<long long, long long>>
-#define vvpll vector<vector<pair<long long, long long>>>
-#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
-#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
-#define setcout(n) cout << setprecision(n) << fixed
-#define all(a) (a).begin(), (a).end()
-#define rall(a) (a).rbegin(), (a).rend()
-#define MOD 998244353LL
-#define INF (1LL << 60)
-//#pragma GCC target("avx2")
-//#pragma GCC optimize("O3")
-//#pragma GCC optimize("unroll-loops")
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
 template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
-int main() {
+int N;
+//---------------------------------------------------------------------------------------------------
+string buf = "";
+void dfs(int cu)
+{
+    if (cu == N)
+    {
+        printf("%s\n", buf.c_str());
+        return;
+    }
 
+    char biggest = *max_element(all(buf));
+
+    for (char c = 'a'; c <= biggest + 1; c++)
+    {
+        buf.push_back(c);
+        dfs(cu + 1);
+        buf.pop_back();
+    }
+}
+//---------------------------------------------------------------------------------------------------
+void _main() {
+    cin >> N;
+    dfs(0);
 }
