38520c871f6ac000aaaa37ed8caf66211a9f0ae1
Zobio <soshinito514@gmail.com>
2022-09-23 01:12:05 +0900

    arc20b(全探索)

---
+++ work/arc20b.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define reep(i, a, b) for(long long i = a; i < b; i++)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
+#define setcout(n) cout << setprecision(n) << fixed
+#define YESS {printf("Yes\n"); return 0;}
+#define NOO {printf("No\n"); return 0;}
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define mint modint998244353
+#define INF (1LL << 60)
+#define PI acos(-1.0)
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	//1, 2枚目の種類を指定すれば後は全部決まる
+	//全通り試してもO(CN)で十分間に合う
+	ll n, c; cin >> n >> c;
+	vll a(n); rep(i, n) cin >> a[i];
+	ll ans = INF;
+	reps(c1, 10) reps(c2, 10) {
+		if(c1 == c2) continue;
+		ll cnt = 0;
+		rep(i, n) cnt += a[i] != (i & 1 ? c1 : c2);
+		chmin(ans, cnt * c);
+	}
+	cout << ans << endl;
+}
