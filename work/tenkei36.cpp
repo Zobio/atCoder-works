68752b7a9ae81ae59657088454beee42e1e6320d
Zobio <64645053+Zobio@users.noreply.github.com>
2022-10-03 10:13:30 +0900

    tenkei36(途中)

---
+++ work/tenkei36.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define setcout(n) cout << setprecision(n) << fixed
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define INF (1LL << 60)
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	ll n, q; cin >> n >> q;
+    vll x(n), y(n);
+    rep(i, n) cin >> x[i] >> y[i];
+    ll dim = 2; //次元数
+    vll cand(1 << dim);
+    rep(i, n) rep(bits, 1 << dim) {
+        chmax(cand[i], (bits >> 1 & 1 ? -1 : 1) * x[i] + (bits >> 0 & 1 ? -1 : 1) * y[i]);
+    }
+    rep(_, q) {
+        ll p; cin >> p; p--;
+        ll ans = 0;
+        rep(i, 1 << dim) chmax(ans, abs(x[i] - ))
+    }
+}
