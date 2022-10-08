3bbf69573f96a3b933c7d193b559f474e4adfff4
Zobio <soshinito514@gmail.com>
2022-10-08 20:30:34 +0900

    arc42b(幾何)

---
+++ work/arc42b.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define reep(i, a, b) for(long long i = a; i < b; i++)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
+#define setcout(n) cout << setprecision(n) << fixed
+#define YESS {printf("Yes\n"); return 0;}
+#define NOO {printf("No\n"); return 0;}
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define mint modint998244353
+#define INF (1LL << 60)
+#define PI acos(-1.0)
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+ld calc_dist(ld x0, ld y0, ld p, ld q, ld r) {
+	return abs(p * x0 + q * y0 + r) / sqrtl(p * p + q * q);
+}
+
+int main() {
+	setcout(15);
+	ld x, y; cin >> x >> y;
+	ll n; cin >> n;
+	vector<pair<ld, ld>> cor(n); rep(i, n) cin >> cor[i].first >> cor[i].second;
+	ld ans = 10100;
+	rep(i, n) {
+		ll i1 = i, i2 = (i + 1) % n;
+		ld a = cor[i1].first, b = cor[i1].second, c = cor[i2].first, d = cor[i2].second;
+		if(a == c) chmin(ans, abs(x - cor[i1].first));
+		else{
+			ld p = (d - b) / (c - a), q = -1, r = -((d - b) / (c - a)) * a + b;
+			chmin(ans, calc_dist(x, y, p, q, r));
+		}
+	}
+	cout << ans << endl;
+}
