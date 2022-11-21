a8f73ba89bcc45b0a8fc956cfd39c64582f7471f
Zobio <soshinito514@gmail.com>
2022-11-21 14:30:15 +0900

    agc32b

---
+++ work/agc32b.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+#define uint unsigned int
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define reep(i, a, b) for(long long i = a; i < b; i++)
+#define fore(i, a) for (auto& i : a)
+#define vll vector<long long>
+#define vvll vector<vector<long long>>
+#define vvvll vector<vector<vector<long long>>>
+#define vvvvll vector<vector<vector<vector<long long>>>>
+#define dll deque<long long>
+#define ddll deque<dque<long long>>
+#define dddll deque<deque<deque<long long>>>
+#define ddddll deque<deque<deque<deque<long long>>>>
+#define pll pair<long long, long long>
+#define vpll vector<pair<long long, long long>>
+#define vvpll vector<vector<pair<long long, long long>>>
+#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
+#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
+#define setcout(n) cout << setprecision(n) << fixed
+#define YESS {printf("Yes\n"); return 0;}
+#define NOO {printf("No\n"); return 0;}
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define MOD 998244353LL
+#define mint modint998244353
+#define INF (1LL << 60)
+#define PI acos(-1.0)
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int main() {
+	//頂点集合を2つ以上の和が一定のグループに分けて、そこのグループ同士に辺をはる
+	ll n; cin >> n;
+	vvll gs;
+	if(n % 2 == 0) rep(i, n / 2) gs.push_back({i, n - 1 - i});
+	else {
+		rep(i, n / 2) gs.push_back({i, n - 2 - i});
+		gs.push_back({n - 1});
+	}
+	ll sum = 0;
+	rep(i, gs.size()) reep(j, i + 1, gs.size()) sum += gs[i].size() * gs[j].size();
+	cout << sum << endl;
+	rep(i, gs.size()) reep(j, i + 1, gs.size()) {
+		for(auto a1 : gs[i]) for(auto a2 : gs[j]) cout << a1 + 1 << " " << a2 + 1 << endl;
+	}
+}
