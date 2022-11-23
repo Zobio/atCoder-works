96850e45573369094a945e92b06453a8cd5ab9b1
Zobio <soshinito514@gmail.com>
2022-11-24 03:36:25 +0900

    abc150d

--- work/test.cpp
+++ work/test.cpp
@@ -1,45 +1,51 @@
-#include <bits/stdc++.h>
-#include <atcoder/all>
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a)
+#define all(x) (x).begin(),(x).end()
+//#pragma GCC optimize ("-O3")
 using namespace std;
-using namespace atcoder;
-#define uint unsigned int
-#define ll long long
-#define ull unsigned long long
-#define ld long double
-#define rep(i, n) for (long long i = 0; i < n; i++)
-#define reps(i, n) for (long long i = 1; i <= n; i++)
-#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
-#define rreps(i, n) for (long long i = n; i >= 1; i--)
-#define reep(i, a, b) for(long long i = a; i < b; i++)
-#define fore(i, a) for (auto& i : a)
-#define vll vector<long long>
-#define vvll vector<vector<long long>>
-#define vvvll vector<vector<vector<long long>>>
-#define vvvvll vector<vector<vector<vector<long long>>>>
-#define dll deque<long long>
-#define ddll deque<dque<long long>>
-#define dddll deque<deque<deque<long long>>>
-#define ddddll deque<deque<deque<deque<long long>>>>
-#define pll pair<long long, long long>
-#define vpll vector<pair<long long, long long>>
-#define vvpll vector<vector<pair<long long, long long>>>
-#define arrcout(a) for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl
-#define arrcout2(a) for(size_t i = 0; i < a.size(); i++) {for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;}
-#define setcout(n) cout << setprecision(n) << fixed
-#define YESS {printf("Yes\n"); return 0;}
-#define NOO {printf("No\n"); return 0;}
-#define all(a) (a).begin(), (a).end()
-#define rall(a) (a).rbegin(), (a).rend()
-#define MOD 998244353LL
-#define mint modint998244353
-#define INF (1LL << 60)
-#define PI acos(-1.0)
-//#pragma GCC target("avx2")
-//#pragma GCC optimize("O3")
-//#pragma GCC optimize("unroll-loops")
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int inf = INT_MAX / 2; const ll infl = 1LL << 60;
 template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
 template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
-int main() {
-  vvll a(300 * 300, vll(300 * 300));
+int N, M, A[101010], B[101010];
+//---------------------------------------------------------------------------------------------------
+int naive() {
+    int ans = 0;
+    
+    rep(x, 1, M + 1) {
+        bool ok = true;
+        rep(i, 0, N) {
+            if (x % B[i] != 0) ok = false;
+            if ((x / B[i]) % 2 == 0) ok = false;
+        }
+        if (ok) {
+            printf(">> %d\n", x);
+            ans++;
+        }
+    }
+
+    return ans;
+}
+//---------------------------------------------------------------------------------------------------
+int solve() {
+    ll lc = 1;
+    rep(i, 0, N) lc = lcm(lc, B[i]);
+
+    rep(i, 0, N) if ((lc / B[i]) % 2 == 0) return 0;
+
+    ll ans = M / lc;
+    ans = (ans + 1) / 2;
+    return ans;
+}
+//---------------------------------------------------------------------------------------------------
+void _main() {
+    cin >> N >> M;
+    rep(i, 0, N) cin >> A[i];
+    rep(i, 0, N) B[i] = A[i] / 2;
+
+    //cout << naive() << endl;
+    cout << solve() << endl;
 }
