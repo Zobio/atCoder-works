b07db6dd7b4d78399cc0f44580ef1733e4b1fa0d
Zobio <soshinito514@gmail.com>
2022-12-05 17:58:36 +0900

    weighted_unionfindライブラリ

--- work/test.cpp
+++ work/test.cpp
@@ -34,6 +34,7 @@ using vvpll = vector<vector<pair<long long, long long>>>;
 #define YES printf("Yes\n"), exit(0)
 #define NO printf("No\n"), exit(0)
 constexpr long long MOD = 998244353LL;
+constexpr long long INF = (1LL << 60);
 template<class T> void setcout(T n) {cout << setprecision(n) << fixed;}
 template<class T> void arrcout(T &a) { for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl; }
 template<class T> void arrcout2(T &a) { for(size_t i = 0; i < a.size(); i++) { for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;} }
@@ -42,56 +43,58 @@ template<class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; }
 //#pragma GCC target("avx2")
 //#pragma GCC optimize("O3")
 //#pragma GCC optimize("unroll-loops")
-const long long INF=(long long)1e18;
 
-int main(){
-	int n,m,q;
-	cin >> n >> m >> q;
-	
-	atcoder::dsu d(n);
-	
-	vector<vector<array<int,2>>>G(n);
-	for(int i=0;i<m;i++){
-		int a,b,c;
-		cin >> a >> b >> c;
-		a--,b--;
-		d.merge(a,b);
-		G[a].push_back({b,c});
-		G[b].push_back({a,-c});
+template<class Abel>
+struct UnionFind {
+	vector<int> par;
+	vector<int> rank;
+	vector<Abel> diff_weight;
+
+	UnionFind(int n = 1, Abel SUM_UNITY = 0) {
+		init(n, SUM_UNITY);
 	}
-	
-	vector<long long>dist(n,INF);
-	vector<bool>negative_loop(n);
-	
-	for(int i=0;i<n;i++)if(d.leader(i)==i){
-		//bfs
-		queue<int>q;
-		dist[i]=0;
-		q.push(i);
-		while(!q.empty()){
-			int v=q.front();q.pop();
-			for(auto[vv,c]:G[v]){
-				if(dist[vv]==INF){
-					dist[vv]=dist[v]+c;
-					q.push(vv);
-				}else{
-					if(dist[vv]!=dist[v]+c)negative_loop[i]=true;
-				}
-			}
+
+	void init(int n = 1, Abel SUM_UNITY = 0) {
+		par.resize(n); rank.resize(n); diff_weight.resize(n);
+		for (int i = 0; i < n; ++i) par[i] = i, rank[i] = 0, diff_weight[i] = SUM_UNITY;
+	}
+
+	int root(int x) {
+		if (par[x] == x) {
+			return x;
+		}
+		else {
+			int r = root(par[x]);
+			diff_weight[x] += diff_weight[par[x]];
+			return par[x] = r;
 		}
 	}
-	
-	for(int i=0;i<q;i++){
-		int x,y;
-		cin >> x >> y;
-		x--,y--;
-		if(!d.same(x,y)){
-			cout << "nan" << endl;
-		}else if(negative_loop[d.leader(x)]){
-			cout << "inf" << endl;
-		}else{
-			cout << dist[y]-dist[x] << endl;
-		}
+
+	Abel weight(int x) {
+		root(x);
+		return diff_weight[x];
 	}
-	arrcout(negative_loop);
+
+	bool issame(int x, int y) {
+		return root(x) == root(y);
+	}
+
+	bool merge(int x, int y, Abel w) {
+		w += weight(x); w -= weight(y);
+		x = root(x); y = root(y);
+		if (x == y) return false;
+		if (rank[x] < rank[y]) swap(x, y), w = -w;
+		if (rank[x] == rank[y]) ++rank[x];
+		par[y] = x;
+		diff_weight[y] = w;
+		return true;
+	}
+
+	Abel diff(int x, int y) {
+		return weight(y) - weight(x);
+	}
+};
+
+int main() {
+	
 }
