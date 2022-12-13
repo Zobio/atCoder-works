4ae778f8dbf653c4e38df967f84810a1bba840cc
Zobio <soshinito514@gmail.com>
2022-12-13 20:29:05 +0900

    水バチャ12/13

---
+++ work/abc249d6.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+#include <atcoder/all> // AtCoder
+using namespace std;
+using namespace atcoder; // AtCoder
+using uint = unsigned int;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using mint = modint998244353; // AtCoder
+using vll = vector<long long>;
+using vvll = vector<vector<long long>>;
+using vvvll = vector<vector<vector<long long>>>;
+using vvvvll = vector<vector<vector<vector<long long>>>>;
+using vbl = vector<bool>;
+using vvbl = vector<vector<bool>>;
+using vvvbl = vector<vector<vector<bool>>>;
+using vvvvbl = vector<vector<vector<vector<bool>>>>;
+using dll = deque<long long>;
+using ddll = deque<deque<long long>>;
+using dddll = deque<deque<deque<long long>>>;
+using ddddll = deque<deque<deque<deque<long long>>>>;
+using pll = pair<long long, long long>;
+using vpll = vector<pair<long long, long long>>;
+using vvpll = vector<vector<pair<long long, long long>>>;
+#define rep(i, n) for (long long i = 0; i < n; i++)
+#define reps(i, n) for (long long i = 1; i <= n; i++)
+#define rrep(i, n) for (long long i = n - 1; i >= 0; i--)
+#define rreps(i, n) for (long long i = n; i >= 1; i--)
+#define reep(i, a, b) for (long long i = a; i < b; i++)
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define PI acos(-1.0)
+#define YES printf("YES\n"), exit(0)
+#define NO printf("NO\n"), exit(0)
+#define Yes printf("Yes\n"), exit(0)
+#define No printf("No\n"), exit(0)
+constexpr long long MOD = 998244353LL;
+constexpr long long INF = (1LL << 60);
+template<class T> void setcout(T n) {cout << setprecision(n) << fixed;}
+template<class T> void arrcout(T &a) { for(size_t i = 0; i < a.size(); i++) cout << (i ? " " : "") << a.at(i); cout << endl; }
+template<class T> void arrcout2(T &a) { for(size_t i = 0; i < a.size(); i++) { for(size_t j = 0; j < a[i].size(); j++) cout << (j ? " " : "") << a.at(i).at(j); cout << endl;} }
+template<class... T> constexpr auto min(T... a){return min(initializer_list<common_type_t<T...>>{a...});}
+template<class... T> constexpr auto max(T... a){return max(initializer_list<common_type_t<T...>>{a...});}
+template<class T> bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+//#pragma GCC target("avx2")
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("unroll-loops")
+
+vector<ll> enum_divisors(ll n) {
+	vector<ll> res;
+	for (ll i = 1; i * i <= n; i++) {
+		if (n % i == 0) {
+			res.push_back(i);
+			if (n / i != i) res.push_back(n / i);
+		}
+	}
+	sort(all(res));
+	return res;
+}
+
+int main() {
+	ll ma = 200010;
+	ll n; cin >> n;
+	vll c(ma);
+	rep(i, n) {
+		ll t; cin >> t;
+		c[t]++;
+	}
+	ll ans = 0;
+	reps(i, 200000) {
+		vll d = enum_divisors(i);
+		for(auto au : d) ans += c[i] * c[au] * c[i / au];
+	}
+	cout << ans << endl;
+}
