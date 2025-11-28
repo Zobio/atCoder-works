ec4c8d280227bd8e1586f0eb9626076e89b97697
Zobio <soshinito514@gmail.com>
2025-11-28 19:24:48 +0900

    abc223d(upsolve)

--- work/test.py
+++ work/test.py
@@ -1,8 +1,5 @@
-n = int(input())
-a = map(int, input().split())
-sum = 0
-ave = 0.0
-for i in range(n):
-	sum += a[i]
-	ave += a[i] / n
-print(sum, ave)
+def add_numbers(x, y):
+    return x + y
+
+a, b = map(int, input().split())
+print(add_numbers(a, b))
